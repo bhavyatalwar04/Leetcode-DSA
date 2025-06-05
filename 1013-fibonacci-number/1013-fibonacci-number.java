3cfff9bddd92a3e6b772ee7cf82783730f299404
Bhavya Talwar <140486068+bhavyatalwar04@users.noreply.github.com>
2025-06-05 15:24:48 +0530

    Time: 0 ms (100%), Space: 40.3 MB (80.51%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -7,7 +7,13 @@ class Solution {
         if(n==1){
             return 1;
         }
-            return fib(n-1)+fib(n-2);
-        
+        int a=0,b=1;
+        for(int i=2;i<=n;i++){
+            int temp=b;
+            b=a+b;
+            a=temp;
+
+        }
+        return b;
     }
 }
