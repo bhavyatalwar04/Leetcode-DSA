0f693e4c568717a20e72b40fb802df7c38a80778
Bhavya Talwar <140486068+bhavyatalwar04@users.noreply.github.com>
2025-07-16 11:38:55 +0530

    Time: 2 ms (80.44%), Space: 46.2 MB (39.65%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,6 +1,6 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        
+
         int n=nums.length;
         int i=0;
 
@@ -8,8 +8,11 @@ class Solution {
             if(nums[j]!=0){
                 swap(nums,i,j);
                 i++;
+                
             }
         }
+        
+    
     }
     public void swap(int[] nums,int i,int j){
         int temp=nums[i];
