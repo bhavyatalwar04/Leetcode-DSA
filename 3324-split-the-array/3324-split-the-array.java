f1bf772ff5def7b3008c74b940265445dbe96f03
Bhavya Talwar <140486068+bhavyatalwar04@users.noreply.github.com>
2025-09-16 16:41:00 +0530

    Time: 3 ms (24.8%), Space: 43.2 MB (30.76%) - LeetHub

---
+++ 3324-split-the-array/3324-split-the-array.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isPossibleToSplit(int[] nums) {
+        
+        HashMap<Integer,Integer> count=new HashMap<>();
+
+        if(nums.length%2!=0) return false;
+
+        for(int n : nums){
+            count.put(n,count.getOrDefault(n,0)+1);
+        }
+
+        for(int val:count.values()){
+            if(val>2){
+                return false;
+            }
+        }
+        return true;
+
+
+
+    }
+}
