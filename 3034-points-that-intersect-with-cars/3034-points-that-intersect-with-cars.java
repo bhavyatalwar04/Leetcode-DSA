db5d1b973876c76a03caea412ab542486bf49f6d
Bhavya Talwar <140486068+bhavyatalwar04@users.noreply.github.com>
2025-10-29 09:55:36 +0530

    Time: 5 ms (33.39%), Space: 44.5 MB (60.71%) - LeetHub

--- 3034-points-that-intersect-with-cars/3034-points-that-intersect-with-cars.java
+++ 3034-points-that-intersect-with-cars/3034-points-that-intersect-with-cars.java
@@ -1,16 +1,24 @@
 class Solution {
     public int numberOfPoints(List<List<Integer>> nums) {
-        
-        Set<Integer> covered=new HashSet<>();
+        Collections.sort(nums,(a,b) -> a.get(0)-b.get(0));
 
-        for(List<Integer> car:nums){
-            int start=car.get(0);
-            int end=car.get(1);
+        int covered=0;
+        int start=nums.get(0).get(0);
+        int end=nums.get(0).get(1);
 
-            for(int i=start;i<=end;i++){
-                covered.add(i);
+        for(int i=1;i<nums.size();i++){
+            int s=nums.get(i).get(0);
+            int e=nums.get(i).get(1);
+
+            if(s<=end){
+                end=Math.max(end,e);
+            }else{
+            covered+=end-start+1;
+            start=s;
+            end=e;
             }
         }
-        return covered.size();        
+        covered+=end-start+1;
+        return covered;
     }
 }
