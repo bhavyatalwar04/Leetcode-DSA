a9cbe49723bc9c852e9bfd3e59c872db04fdab58
Bhavya Talwar <140486068+bhavyatalwar04@users.noreply.github.com>
2026-02-04 12:00:43 +0530

    Time: 1 ms (71.97%), Space: 45 MB (10.46%) - LeetHub

--- 3637-trionic-array-i/3637-trionic-array-i.java
+++ 3637-trionic-array-i/3637-trionic-array-i.java
@@ -1,23 +1,22 @@
 class Solution {
     public boolean isTrionic(int[] nums) {
-
         int n = nums.length;
         if (n < 4)
             return false;
 
         int state = 0;
-        for (int i = 0; i < n-1; i++) {
+        for (int i = 0; i < n - 1; i++) {
             if (state == 0) {
                 if (nums[i] < nums[i + 1]) {
                     //increasing
-                } else if (i > 0 && nums[i] > nums[i + 1]) {
+                } else if (i>0 && nums[i] > nums[i + 1]) {
                     state = 1;
                 } else {
                     return false;
                 }
             } else if (state == 1) {
                 if (nums[i] > nums[i + 1]) {
-
+                    //decreasing
                 } else if (nums[i] < nums[i + 1]) {
                     state = 2;
                 } else {
@@ -25,12 +24,12 @@ class Solution {
                 }
             } else if (state == 2) {
                 if (nums[i] < nums[i + 1]) {
-
+                    //increasing
                 } else {
                     return false;
                 }
             }
         }
-        return state==2;
+        return state == 2;
     }
 }
