aa42d89b8fb399385e6aa274f7c795e0e46aa8bf
Bhavya Talwar <140486068+bhavyatalwar04@users.noreply.github.com>
2026-06-23 12:32:40 +0530

    Time: 130 ms (78.33%), Space: 111.5 MB (36.7%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
@@ -1,30 +1,32 @@
 class MedianFinder {
-
     PriorityQueue<Integer>minHeap=new PriorityQueue<>();
     PriorityQueue<Integer>maxHeap=new PriorityQueue<>(Collections.reverseOrder());
     public MedianFinder() {
-
+        
     }
-
+    
     public void addNum(int num) {
-        if(maxHeap.size()==0) maxHeap.offer(num);
+        if(maxHeap.isEmpty()) maxHeap.offer(num);
         else{
-            if(num<maxHeap.peek()) 
-                maxHeap.offer(num);
+            if(num<maxHeap.peek()) maxHeap.offer(num);
             else minHeap.offer(num);
         }
-        if(maxHeap.size()==minHeap.size()+2) minHeap.add(maxHeap.remove());
-        if(minHeap.size()==maxHeap.size()+2) maxHeap.add(minHeap.remove());
+        if(maxHeap.size()==minHeap.size()+2){
+            minHeap.offer(maxHeap.poll());
+        }else if(minHeap.size()==maxHeap.size()+2){
+            maxHeap.offer(minHeap.poll());
+        }
+        
     }
-
+    
     public double findMedian() {
-        if (maxHeap.size() == minHeap.size() + 1) {
+        if(maxHeap.size()==minHeap.size()+1){
             return maxHeap.peek();
-        }
-        else if (minHeap.size() == maxHeap.size() + 1) {
+        }else if(minHeap.size()==maxHeap.size()+1){
             return minHeap.peek();
-        }
-        else return (maxHeap.peek()+minHeap.peek())/2.0;
+        }else{
+            return (maxHeap.peek()+minHeap.peek())/2.0;
+        } 
     }
 }
 
