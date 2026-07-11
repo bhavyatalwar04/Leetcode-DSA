6f42667fd66053dfec0263eae082af603075cbf0
Bhavya Talwar <140486068+bhavyatalwar04@users.noreply.github.com>
2026-07-11 15:55:38 +0530

    Time: 1 ms (95.83%), Space: 46.2 MB (86.03%) - LeetHub

---
+++ 2596-check-knight-tour-configuration/2596-check-knight-tour-configuration.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean checkValidGrid(int[][] grid) {
+        int n = grid.length;
+        if (grid[0][0] != 0)
+            return false;
+        return isValid(grid, 0, 0, n, 0);
+    }
+
+    boolean isValid(int[][]grid, int r, int c, int n, int expVal){
+        if(r<0||c<0||r>=n||c>=n|| grid[r][c]!=expVal) return false;
+        if(expVal==n*n-1) return true;
+
+        return isValid(grid,r-2,c+1,n,expVal+1)||
+               isValid(grid,r-1,c+2,n,expVal+1)||
+               isValid(grid,r+1,c+2,n,expVal+1)||
+               isValid(grid,r+2,c+1,n,expVal+1)||
+               isValid(grid,r+2,c-1,n,expVal+1)||
+               isValid(grid,r+1,c-2,n,expVal+1)||
+               isValid(grid,r-1,c-2,n,expVal+1)||
+               isValid(grid,r-2,c-1,n,expVal+1);
+    }
+}
