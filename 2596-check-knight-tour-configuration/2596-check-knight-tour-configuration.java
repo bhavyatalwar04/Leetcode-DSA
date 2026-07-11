b928f0a21f765852f048edb8dd3bec32f3db0c7f
Bhavya Talwar <140486068+bhavyatalwar04@users.noreply.github.com>
2026-07-11 15:55:51 +0530

    Time: 0 ms (100%), Space: 46.2 MB (70.18%) - LeetHub

--- 2596-check-knight-tour-configuration/2596-check-knight-tour-configuration.java
+++ 2596-check-knight-tour-configuration/2596-check-knight-tour-configuration.java
@@ -6,17 +6,19 @@ class Solution {
         return isValid(grid, 0, 0, n, 0);
     }
 
-    boolean isValid(int[][]grid, int r, int c, int n, int expVal){
-        if(r<0||c<0||r>=n||c>=n|| grid[r][c]!=expVal) return false;
-        if(expVal==n*n-1) return true;
+    boolean isValid(int[][] grid, int r, int c, int n, int expVal) {
+        if (r < 0 || c < 0 || r >= n || c >= n || grid[r][c] != expVal)
+            return false;
+        if (expVal == n * n - 1)
+            return true;
 
-        return isValid(grid,r-2,c+1,n,expVal+1)||
-               isValid(grid,r-1,c+2,n,expVal+1)||
-               isValid(grid,r+1,c+2,n,expVal+1)||
-               isValid(grid,r+2,c+1,n,expVal+1)||
-               isValid(grid,r+2,c-1,n,expVal+1)||
-               isValid(grid,r+1,c-2,n,expVal+1)||
-               isValid(grid,r-1,c-2,n,expVal+1)||
-               isValid(grid,r-2,c-1,n,expVal+1);
+        return isValid(grid, r - 2, c + 1, n, expVal + 1) ||
+                isValid(grid, r - 1, c + 2, n, expVal + 1) ||
+                isValid(grid, r + 1, c + 2, n, expVal + 1) ||
+                isValid(grid, r + 2, c + 1, n, expVal + 1) ||
+                isValid(grid, r + 2, c - 1, n, expVal + 1) ||
+                isValid(grid, r + 1, c - 2, n, expVal + 1) ||
+                isValid(grid, r - 1, c - 2, n, expVal + 1) ||
+                isValid(grid, r - 2, c - 1, n, expVal + 1);
     }
 }
