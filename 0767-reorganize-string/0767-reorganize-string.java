3bfb1a7cb7c68f9f4af257fc27c6c6644bc6d42c
Bhavya Talwar <140486068+bhavyatalwar04@users.noreply.github.com>
2026-07-15 11:03:57 +0530

    Time: 3 ms (75.3%), Space: 43.1 MB (38.28%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.java
+++ 0767-reorganize-string/0767-reorganize-string.java
@@ -1,28 +1,27 @@
 class Solution {
     public String reorganizeString(String s) {
-        int n=s.length();
-        int[]freq=new int[26];
-        for(char c:s.toCharArray()){
-            freq[c-'a']++;
+        int n = s.length();
+        int[] freq = new int[26];
+        for (char ch : s.toCharArray()) {
+            freq[ch - 'a']++;
         }
-        PriorityQueue<int[]>pq=new PriorityQueue<>((a,b)->b[1]-a[1]);
 
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[1] - a[1]);
         for(int i=0;i<26;i++){
             if(freq[i]>(n+1)/2) return "";
             if(freq[i]>0) pq.offer(new int[]{i,freq[i]});
         }
-        StringBuilder ans=new StringBuilder();
+        StringBuilder sb=new StringBuilder();
         while(pq.size()>=2){
             int[]first=pq.poll();
             int[]second=pq.poll();
-
-            ans.append((char)(first[0]+'a'));
-            ans.append((char)(second[0]+'a'));
-
+            sb.append((char)(first[0]+'a'));
+            sb.append((char)(second[0]+'a'));
             if(--first[1]>0) pq.offer(first);
             if(--second[1]>0) pq.offer(second);
         }
-        if(!pq.isEmpty()) ans.append((char)(pq.poll()[0]+'a'));
-        return ans.toString();
+        if(!pq.isEmpty()) sb.append((char)(pq.poll()[0]+'a'));
+        return sb.toString();
+
     }
 }
