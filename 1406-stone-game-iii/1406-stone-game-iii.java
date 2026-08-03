6156ab1b5dbb01a95ccb660dfbfe8b7d196624a9
Bhavya Talwar <140486068+bhavyatalwar04@users.noreply.github.com>
2026-08-03 12:55:40 +0530

    Time: 48 ms (25.57%), Space: 95.1 MB (15.06%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.java
+++ 1406-stone-game-iii/1406-stone-game-iii.java
@@ -1,8 +1,7 @@
 class Solution {
     public String stoneGameIII(int[] stones) {
         int n = stones.length;
-        int[] dp = new int[n];
-        Arrays.fill(dp,-1);
+        Integer[] dp = new Integer[n];
         int diff = solve(stones, 0, n, dp);
 
         if (diff < 0)
@@ -13,16 +12,16 @@ class Solution {
             return "Tie";
     }
 
-    int solve(int[] stones, int i, int n,int[]dp) {
+    int solve(int[] stones, int i, int n, Integer[] dp) {
         if (i >= n)
             return 0;
-        if (dp[i] != -1)
+        if (dp[i] != null)
             return dp[i];
         int res = stones[i] - solve(stones, i + 1, n, dp);
         if (i + 1 < n)
-            res = Math.max(res, stones[i] + stones[i + 1] - solve(stones, i + 2, n,dp));
+            res = Math.max(res, stones[i] + stones[i + 1] - solve(stones, i + 2, n, dp));
         if (i + 2 < n)
-            res = Math.max(res, stones[i] + stones[i + 1] + stones[i + 2] - solve(stones, i + 3, n,dp));
+            res = Math.max(res, stones[i] + stones[i + 1] + stones[i + 2] - solve(stones, i + 3, n, dp));
         return dp[i] = res;
     }
 }
